0984d0f0ec0a9a5c07003cfae645138a7a6f928f
Harun Taha Kepenek <hakukpnk@gmail.com>
2026-05-18 19:09:19 +0300

    project initialized

---
+++ wind-turbine-api/src/main/java/com/windturbine/wind_turbine_api/WindTurbineApiApplication.java
@@ -0,0 +1,13 @@
+package com.windturbine.wind_turbine_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindTurbineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindTurbineApiApplication.class, args);
+	}
+
+}
